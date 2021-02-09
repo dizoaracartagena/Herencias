8fc759cdd28a9aebd2d239bb015460ae4f002a4f
Dizoara Matías Cartagena <dizoaracartagena@gmail.com>
2021-02-09 15:59:06 -0600

    Add files via upload

---
+++ src/test/TestHerencia.java
@@ -0,0 +1,18 @@
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import domain.Persona;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 5000.0);
+        System.out.println("empleado1 = " + empleado1);
+//        var fecha = new Date();
+//        Cliente cliente1 = new Cliente (fecha, "Karla", 'F', 18, "Avenida Coco", false);
+//        System.out.println("cliente1 = " + cliente1);
+        
+//        Persona persona1 = new Persona();
+    }
+}
